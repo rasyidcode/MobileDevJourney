0e1b8c53d84ca931b45bcd11e5085d3ab7a26a63
rasyidcode <rasyidcode7@gmail.com>
2024-04-25 21:54:16 +0700

    add the solution

--- _NewJourney/Lemonade/app/src/main/java/com/rasyidcode/lemonade/MainActivity.kt
+++ _NewJourney/Lemonade/app/src/main/java/com/rasyidcode/lemonade/MainActivity.kt
@@ -25,12 +25,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
